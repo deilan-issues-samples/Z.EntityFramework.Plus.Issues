51d60fde481a5ca3bd2afadd3da318873a9aad54
Deilan <deilan@users.noreply.github.com>
2018-08-09 12:36:29 +0300

    Add project files.

---
+++ Z.EntityFramework.Plus.Issues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Z.EntityFramework.Plus.Issues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
